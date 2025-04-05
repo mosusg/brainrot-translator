47d55327aa2aa560f4e31b4ef6383f2dcae556d4
Molkly <161694641+Molkly@users.noreply.github.com>
2025-04-05 15:35:33 -0700

    ui ahh

---
+++ scripts/app.gd
@@ -0,0 +1,3 @@
+extends Control
+
+		get_tree().change_scene_to_file("res://scenes/menu.tscn")
--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,13 +1,13 @@
 extends Control
 
 
-func _on_start_pressed() -> void:
+
 		get_tree().change_scene_to_file("res://scenes/app.tscn")
 
 
-func _on_settings_pressed() -> void:
+
 		get_tree().change_scene_to_file("res://scenes/settings.tscn")
 
 
-func _on_quit_pressed() -> void:
+
 		get_tree().quit()
