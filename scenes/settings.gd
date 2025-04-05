0ea6de051dab347b01ac1717c451d11fb696c60b
Molkly <161694641+Molkly@users.noreply.github.com>
2025-04-05 13:55:12 -0700

    buttons ahh

---
+++ scenes/settings.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_option_1_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_option_2_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_main_menusettings_pressed() -> void:
+		get_tree().change_scene_to_file("res://scenes/menu.tscn")
--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,13 +1,13 @@
 extends Control
 
-#start
+
 func _on_start_pressed() -> void:
 		get_tree().change_scene_to_file("res://scenes/app.tscn")
-		
-#settings
-func _on_settings_pressed() -> void:
-	pass # Replace with function body.
 
-#quit
+
+func _on_settings_pressed() -> void:
+		get_tree().change_scene_to_file("res://scenes/settings.tscn")
+
+
 func _on_quit_pressed() -> void:
 		get_tree().quit()
