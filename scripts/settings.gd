5a152c050b97a4f314d0b6368c873402271664a6
Molkly <161694641+Molkly@users.noreply.github.com>
2025-04-06 04:52:47 -0700

    tralalala

--- scripts/settings.gd
+++ scripts/settings.gd
@@ -1,10 +1,20 @@
 extends Control
 
+@onready var sprite = $no  # Your Sprite node
+@onready var timer = $no/Timer  # Your Timer node
+@onready var button = $"CenterContainer/VBoxContainer2/Option 1"  # Your Button node
 
+func _ready() -> void:
+	sprite.visible = false
+	
 func _on_option_1_pressed() -> void:
-	pass
+	sprite.visible = true
+	timer.start(2.0)
 
-
+func _on_timer_timeout():
+	# Hide the sprite when the timer times out
+	sprite.visible = false
+	
 func _on_option_2_pressed() -> void:
 	pass
 
