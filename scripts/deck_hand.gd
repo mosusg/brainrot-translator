e6317d22597c29cc50c57b526a8081cf760f31b4
Molkly <161694641+Molkly@users.noreply.github.com>
2025-04-06 03:53:26 -0700

    good lookin

--- scripts/deck_hand.gd
+++ scripts/deck_hand.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
-@onready var attack_card_scene: PackedScene = preload("res://scenes/cards/attack_card.tscn")
-@onready var defend_card_scene: PackedScene = preload("res://scenes/cards/defend_card.tscn")
+@onready var rizz_card_scene: PackedScene = preload("res://scenes/cards/rizz_card.tscn")
+@onready var mog_card_scene: PackedScene = preload("res://scenes/cards/mog_card.tscn")
+@onready var aura_card_scene: PackedScene = preload("res://scenes/cards/aura_card.tscn")
 
 @onready var hand: Hand = $CanvasLayer/Hand
 
@@ -11,12 +12,18 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-func _on_card_test_pressed() -> void:
-	var attack_card = attack_card_scene.instantiate()
-	hand.add_card(attack_card)
-	print("Attack card added")
+func _on_rizz_pressed() -> void:
+	var rizz_card = rizz_card_scene.instantiate()
+	hand.add_card(rizz_card)
+	print("rizz card added")
 
-func _on_card_test_2_pressed() -> void:
-	var defend_card = defend_card_scene.instantiate()
-	hand.add_card(defend_card)
-	print("Defend card added")
+
+func _on_mog_pressed() -> void:
+	var mog_card = mog_card_scene.instantiate()
+	hand.add_card(mog_card)
+	print("mog card added")
+
+func _on_aura_pressed() -> void:
+	var aura_card = aura_card_scene.instantiate()
+	hand.add_card(aura_card)
+	print("aura card added")
