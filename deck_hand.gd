0753558a02ac3b0c821c24f973afce0ef2fbdbe6
Molkly <161694641+Molkly@users.noreply.github.com>
2025-04-06 00:50:37 -0700

    add to hand ahh

--- deck_hand.gd
+++ deck_hand.gd
@@ -3,14 +3,20 @@ extends Node2D
 @onready var attack_card_scene: PackedScene = preload("res://scenes/cards/attack_card.tscn")
 @onready var defend_card_scene: PackedScene = preload("res://scenes/cards/defend_card.tscn")
 
-@onready var spawn_point = $CanvasLayer/spawn
+@onready var hand: Hand = $CanvasLayer/Hand
 
 func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
 	pass
-	
+
 func _on_card_test_pressed() -> void:
 	var attack_card = attack_card_scene.instantiate()
-	spawn_point.add_child(attack_card)
+	hand.add_card(attack_card)
+	print("Attack card added")
+
+func _on_card_test_2_pressed() -> void:
+	var defend_card = defend_card_scene.instantiate()
+	hand.add_card(defend_card)
+	print("Defend card added")
--- hand.gd
+++ hand.gd
@@ -1,15 +1,45 @@
+# Inside Hand.gd
+
 @tool
-extends Node2D
+class_name Hand extends Node2D
 
 @export var hand_radius: int = 100
+@export var card_angle: float = 90
+@export var angle_limit: float = 25
+@export var max_card_spread_angle: float = 3
 
+@onready var test_card = $test_card
 @onready var collision_shape: CollisionShape2D = $DebugShape
 
-func get_card_position(angle: float):
+var hand: Array = []
+
+func add_card(card: Node2D):
+	hand.push_back(card)
+	add_child(card)
+	reposition_cards()
+
+
+func reposition_cards():
+	var card_spread = min(angle_limit / hand.size(), max_card_spread_angle)
+	var current_angle = -(card_spread * (hand.size() - 1)) / 2 -90
+	for card in hand:
+		_update_card_transform(card, current_angle)
+		current_angle += card_spread
+
+func get_card_position(angle_in_deg: float) -> Vector2:
+	var x: float = hand_radius * cos(deg_to_rad(angle_in_deg))
+	var y: float = hand_radius * sin(deg_to_rad(angle_in_deg))
+	return Vector2(int(x), int(y))
+
+func _update_card_transform(card: Node2D, angle_in_deg: float):
+	card.set_position(get_card_position(angle_in_deg))
+	card.set_rotation(deg_to_rad(angle_in_deg + 90))
+
+func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
-	
-	
 	if (collision_shape.shape as CircleShape2D).radius != hand_radius:
 		(collision_shape.shape as CircleShape2D).set_radius(hand_radius)
+	test_card.set_position(get_card_position(card_angle))
+	test_card.set_rotation(deg_to_rad(card_angle + 90))
